11694d7733b225d7eb976bc7b39248a0a5c59ce8
vrortega <enigmadanceteam@gmail.com>
2024-06-07 11:01:24 -0300

    Initial Commit

---
+++ CampeoesDaCopa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CampeoesDaCopa
+//
+//  Created by Vitoria Ortega on 07/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
