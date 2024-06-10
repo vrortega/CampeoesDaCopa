62635d4c206ca6510f2d72a6a0218f05a5582d19
vrortega <enigmadanceteam@gmail.com>
2024-06-09 22:38:46 -0300

    feat: add data on the game information screen

--- CampeoesDaCopa/Controllers/WorldCupViewController.swift
+++ CampeoesDaCopa/Controllers/WorldCupViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 class WorldCupViewController: UIViewController {
     
     var worldCup: WorldCup!
-
+    @IBOutlet weak var winnerIv: UIImageView!
+    @IBOutlet weak var viceIv: UIImageView!
+    @IBOutlet weak var scoreLb: UILabel!
+    @IBOutlet weak var viceLb: UILabel!
+    @IBOutlet weak var winnerLb: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Ano da copa selecionada: \(worldCup.year)")
+        title = "Worldup \(worldCup.year)"
+        winnerIv.image = UIImage(named: "\(worldCup.winner).png")
+        viceIv.image = UIImage(named: "\(worldCup.vice).png")
+        winnerLb.text = worldCup.winner
+        viceLb.text = worldCup.vice
+        scoreLb.text = "\(worldCup.winnerScore) x \(worldCup.viceScore)"
+
+        
+
     }
 }
